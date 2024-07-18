05f1446501db55bbcb86eba208d588797bedc3e6
Wellington Nascente Hirsch <144137092+wellington-hirsch-zoop@users.noreply.github.com>
2024-07-18 13:13:16 -0300

    #1 first commit with the project base

---
+++ EstaparHelpCenter/EstaparHelpCenter/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  EstaparHelpCenter
+//
+//  Created by Wellington Nascente Hirsch on 18/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
